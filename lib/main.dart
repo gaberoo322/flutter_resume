59b53034412979541e6c3e1ecdd05c118c0e4c3d
gabe greenfield <gabegreenfield@Gabes-Computer.local>
2020-03-10 07:50:28 -0600

    updating main title

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class FlutterResume extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gabe\'s Resume',
       theme: ThemeData(
         primarySwatch: Colors.teal,
         accentColor: Colors.pink,
